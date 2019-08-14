50f7899e0bd22b8755d3209c7585154e0dc0bd17
Ricky Zhuang <hr.zhuang@outlook.com>
2019-08-14 16:14:01 -0400

    Add mobile homepage icons and web app manifest

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 module.exports = {
   module: {
@@ -21,6 +22,27 @@ module.exports = {
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: 'public/index.html',
+    }),
+    new WebpackPwaManifest({
+      name: 'Simple Checklist',
+      short_name: 'Checklist',
+      description: 'A simple checklist app',
+      background_color: '#007bff',
+      theme_color: '#007bff',
+      ios: {
+        'apple-mobile-web-app-status-bar-style': 'black',
+      },
+      icons: [
+        {
+          src: 'assets/ios-icon.png',
+          sizes: [120, 152, 167, 180, 1024],
+          ios: true,
+        },
+        {
+          src: 'assets/android-icon.png',
+          sizes: [36, 48, 72, 96, 144, 192, 512],
+        },
+      ],
     }),
   ],
 };
